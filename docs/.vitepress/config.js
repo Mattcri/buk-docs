b42efb07635e8d64af178f89a59a4b4127adef50
Matias-15 <mcriado.cg@gmail.com>
2022-12-22 16:21:23 -0300

    Git init and first config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  lang: "es-CL",
+  title: "Buk",
+  description: "Documentación y UI Kit web del equipo Marketing Corporativo de Buk, software para Recursos Humanos",
+
+  themeConfig: {
+    siteTitle: "Docs Frontend Marketing Corp",
+    footer: {
+      message: "Hecho con ❤ por el equipo Front Mkt Corp",
+      copyright: "© 2022-presente",
+    }
+  },
+
+}
